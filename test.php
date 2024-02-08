450c1e06beabe895a788dd40e581efe559fabc01
SacramDED <bubles013@gmail.com>
2024-02-08 02:36:31 +0100

    deletions (moved)

---
+++ test.php
@@ -0,0 +1,110 @@
+<?php
+
+function find_differences($obj1, $obj2) {
+    $deletions = [];
+    $changes = [];
+    $additions = [];
+
+    if (count($obj1) != count($obj2)) {
+        $deletions[] = "Objects have different lengths";
+    }
+
+    for ($i = 0; $i < count($obj1); $i++) {
+        // Check if the item ID exists in both objects
+        $id1 = $obj1[$i]['itemId'];
+        $found = false;
+        foreach ($obj2 as $item) {
+            if ($item['itemId'] === $id1) {
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found) {
+            // If the item ID is missing in obj2, consider it deleted
+            $deletions[] = "Item with ID $id1 is deleted";
+            continue; // Skip further comparison for this row
+        }
+
+        // Compare subcategory IDs
+        foreach ($obj1[$i]['subcategories'] as $sub1) {
+            $found = false;
+            foreach ($obj2[$i]['subcategories'] as $sub2) {
+                if ($sub1['subcategoryId'] === $sub2['subcategoryId']) {
+                    $found = true;
+                    break;
+                }
+            }
+            if (!$found && $sub1['subcategoryId'] !== null) {
+                $deletions[] = "Subcategory with ID {$sub1['subcategoryId']} in item with ID $id1 is deleted";
+            }
+        }
+    }
+
+    return [
+        'deletions' => $deletions,
+        'changes' => $changes,
+        'additions' => $additions,
+    ];
+}
+
+// Sample objects
+$obj1 = [
+    [
+        "itemId" => 5,
+        "itemName" => "Dummy Task 1",
+        "subcategories" => [
+            ["subcategoryId" => 13, "subcategoryName" => "Subcategory 3 (Not NULL)", "subcategoryOrder" => 1],
+            ["subcategoryId" => 14, "subcategoryName" => "Subcategory 4 (Not NULL)", "subcategoryOrder" => 2],
+            ["subcategoryId" => 17, "subcategoryName" => "Subcategory 5 (Not NULL)", "subcategoryOrder" => 3]
+        ]
+    ],
+    [
+        "itemId" => 6,
+        "itemName" => "Dummy Task 2",
+        "subcategories" => [
+            ["subcategoryId" => 18, "subcategoryName" => "Subcategory 5 (Not NULL)", "subcategoryOrder" => 1],
+            ["subcategoryId" => 20, "subcategoryName" => "Subcategory 6 (Not NULL)", "subcategoryOrder" => 2]
+        ]
+    ]
+];
+
+$obj2 = [
+    [
+        "itemId" => 5,
+        "itemName" => "Dummy Task 1",
+        "subcategories" => [
+            ["subcategoryId" => 13, "subcategoryName" => "Subcategory 3 (Not NULL)", "subcategoryOrder" => 1],
+            ["subcategoryId" => 14, "subcategoryName" => "Subcategory 4 (Not NULL)", "subcategoryOrder" => 2],
+            ["subcategoryId" => 17, "subcategoryName" => "Subcategory 5 (Not NULL)", "subcategoryOrder" => 3],
+            ["subcategoryId" => 19, "subcategoryName" => "Subcategory 6 (Not NULL)", "subcategoryOrder" => 4]
+        ]
+    ],
+    [
+        "itemId" => 6,
+        "itemName" => "Dummy Task 2",
+        "subcategories" => [
+            ["subcategoryId" => 15, "subcategoryName" => "Subcategory 5 (Not NULL)", "subcategoryOrder" => 1],
+            ["subcategoryId" => 20, "subcategoryName" => "Subcategory 6 (Not NULL)", "subcategoryOrder" => 2]
+        ]
+    ]
+];
+
+$differences = find_differences($obj1, $obj2);
+
+echo "Deletions:\n";
+foreach ($differences['deletions'] as $difference) {
+    echo $difference . "\n";
+}
+
+echo "\nChanges:\n";
+foreach ($differences['changes'] as $difference) {
+    echo $difference . "\n";
+}
+
+echo "\nAdditions:\n";
+foreach ($differences['additions'] as $difference) {
+    echo $difference . "\n";
+}
+
+?>
